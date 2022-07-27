5cebd96b3aadb8523c5752db7794743c64ebc2a8
Enoch Okyere <73598581+Enocho1-1@users.noreply.github.com>
2022-07-27 13:15:40 -0400

    third commit
    side widget
    side-menu

--- script.js
+++ script.js
@@ -8,3 +8,16 @@
     const closeFullImg = () => {
         document.querySelector('#fullImageBox').style.display = 'none'
     }
+
+    document.addEventListener('click', e => {
+        switch(e.target.id){
+            case "widget":
+               const sideMenu = document.querySelector('#side-menu')
+               sideMenu.style.display = 'flex'
+            break
+            case "close":
+                const parent = document.querySelector('#side-menu')
+                parent.style.display = 'none'
+             break
+        }
+    })
