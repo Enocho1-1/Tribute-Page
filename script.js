36c840885a0544856e28d098ba31d6648453e016
Enoch Okyere <73598581+Enocho1-1@users.noreply.github.com>
2022-07-28 12:02:45 -0400

    Second Commit

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
     }
 
     document.addEventListener('click', e => {
+        let overlay = document.querySelector('.page_overlay')
         switch(e.target.id){
             case "widget":
                const sideMenu = document.querySelector('#side-menu')
@@ -18,6 +19,26 @@
             case "close":
                 const parent = document.querySelector('#side-menu')
                 parent.style.display = 'none'
+             break
+             case "close_btn":
+                e.target.parentElement.style.display = 'none'
+                document.querySelectorAll('div.modal').forEach( element => {
+                    element.style.display = 'none'
+                })
+             break
+             case "early_life_button":
+                const sectionOne = document.querySelector('#section_one_modal')
+                 overlay = document.querySelector('.page_overlay')
+
+                sectionOne.style.display = 'block'
+                overlay.style.display = 'block'
+             break
+             case "NOI_button":
+                const sectionTwo = document.querySelector('#section_two_modal')
+                 overlay = document.querySelector('.page_overlay')
+
+                sectionTwo.style.display = 'block'
+                overlay.style.display = 'block'
              break
         }
     })
